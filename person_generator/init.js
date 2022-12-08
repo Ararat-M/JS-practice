ff3f8a344bd8f5b85816c0922895f2201c586611
Ararat-M <melkumyan2077@gmail.com>
2022-12-08 16:53:05 +0300

    update scripts person_generator

--- person_generator/init.js
+++ person_generator/init.js
@@ -6,5 +6,24 @@ window.onload = function () {
     document.querySelector("#middleName").innerText = person.middleName;
     document.querySelector("#gender").innerText = person.gender;
     document.querySelector("#birthday").innerText = person.birthday;
+    if (person.gender == personGenerator.GENDER_MALE) {
+        document.querySelector(".person-card-top").style.backgroundImage = "url(./img/male-icon.svg)"
+    } else {
+        document.querySelector(".person-card-top").style.backgroundImage = "url(./img/female-icon.svg)" 
+    }
     
 }
+
+document.querySelector("#btnGenerate").addEventListener("click", function () {
+    onload();
+})
+
+document.querySelector("#btnClear").addEventListener("click", function () {
+    document.querySelector("#firstName").innerText = "";
+    document.querySelector("#surname").innerText = "";
+    document.querySelector("#middleName").innerText = "";
+    document.querySelector("#gender").innerText = "";
+    document.querySelector("#birthday").innerText = "";
+    document.querySelector(".person-card-top").style.backgroundImage = "url()" 
+})
+
